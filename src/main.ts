45b90af9a05aa3fd44f4772b23eab6802d2c6543
Yeattt <nekz.8896@gmail.com>
2024-04-25 21:19:33 -0500

    Initial project structure

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger: Logger = new Logger('Main-Gateway');
+  
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Gateway running on port ${envs.port}`);
+}
+bootstrap();
